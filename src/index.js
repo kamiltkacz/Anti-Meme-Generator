b13f5ceca0fc0677b804ad416833760f926e01c8
Kamil Tkacz <50182884+kamiltkacz@users.noreply.github.com>
2021-01-27 18:21:19 -0300

    Hello msg

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, Kam, welcome to your first app!');
+ReactDOM.render(element, document.getElementById('root'));
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
