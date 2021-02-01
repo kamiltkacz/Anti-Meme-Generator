3582e755c3b9044d9d3dce14763c9ab8e868ee87
Kamil Tkacz <50182884+kamiltkacz@users.noreply.github.com>
2021-02-01 18:25:07 -0300

    erese desc reoprt web vitals

--- src/index.js
+++ src/index.js
@@ -11,11 +11,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// let element = React.createElement('h1', { className: 'greeting' }, 'Hello, Kam, welcome to your first app!');
-// ReactDOM.render(element, document.getElementById('root'));
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
